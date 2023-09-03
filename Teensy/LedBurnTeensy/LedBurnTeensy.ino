59764d13b30c49a28d65084636730cbaa2b66b25
May Ben Arie <may.benarie@vonage.com>
2023-09-03 11:58:36 +0300

    teensy default animation in case of no network

--- Teensy/LedBurnTeensy/LedBurnTeensy.ino
+++ Teensy/LedBurnTeensy/LedBurnTeensy.ino
@@ -1,6 +1,6 @@
-#include <SPI.h>         // needed for Arduino versions later than 0018
+#include <SPI.h>  // needed for Arduino versions later than 0018
 #include <Ethernet.h>
-#include <EthernetUdp.h>         // UDP library from: bjoern@cs.stanford.edu 12/30/2008
+#include <EthernetUdp.h>  // UDP library from: bjoern@cs.stanford.edu 12/30/2008
 
 #include <OctoWS2811.h>
 
@@ -53,8 +53,8 @@
 //
 
 #define MAX_PIXELS 600
-#define LB_HEADER_SIZE (8+8+8)
-#define MAX_PACKET_SIZE ( LB_HEADER_SIZE + ( 3 * (MAX_PIXELS)))
+#define LB_HEADER_SIZE (8 + 8 + 8)
+#define MAX_PACKET_SIZE (LB_HEADER_SIZE + (3 * (MAX_PIXELS)))
 
 // we add one invisible led due to bug found in octows2811
 #define LEDS_PER_STRIP (MAX_PIXELS + 1)
@@ -68,90 +68,66 @@ OctoWS2811 leds(LEDS_PER_STRIP, displayMemory, drawingMemory, config);
 // Enter a MAC address and IP address for your controller below.
 // The IP address will be dependent on your local network:
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xCB //last byte should be the hex of the last byte of the ip address
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xCB  //last byte should be the hex of the last byte of the ip address
 };
-IPAddress ip( 10, 0, 0, 203 );
-//201=C9 202=CA 203=CB 204=CC 205=CD
-unsigned int localPort = 2000;      // local port to listen on
+IPAddress ip(10, 0, 0, 203);
+unsigned int localPort = 2000;  // local port to listen on
 // An EthernetUDP instance to let us send and receive packets over UDP
 EthernetUDP Udp;
 
 uint32_t currentFrame = 0;
 
-// we support up to 512 segments. it means at least 64 per strip, which is 10 pixels per packet.
-// this is more than enough
-#define MAX_SUPPORTED_SEGMENTS (8 * 64)
-bool receivedSegArr[MAX_SUPPORTED_SEGMENTS] = {false};
-uint32_t numOfReceivedSegments = 0;
-
-struct PacketHeaderData
-{
+struct PacketHeaderData {
   uint32_t frameId;
   uint32_t segInFrame;
   uint32_t currSegId;
   uint16_t stripId;
   uint16_t pixelId;
-  uint16_t numOfPixels; // this is not actually header data, but it's nice to have it here
+  uint16_t numOfPixels;  // this is not actually header data, but it's nice to have it here
 };
+const PacketHeaderData EmptyPacketHeaderData = {};
+
+// we support up to 512 segments. it means at least 64 per strip, which is 10 pixels per packet.
+// this is more than enough
+#define MAX_SUPPORTED_SEGMENTS (8 * 4)
+PacketHeaderData receivedSegArr[MAX_SUPPORTED_SEGMENTS] = { EmptyPacketHeaderData };
+bool receivedSegArrBool[MAX_SUPPORTED_SEGMENTS] = { false };
+uint32_t numOfReceivedSegments = 0;
 
 //////////////////////////////////////////////////////////
-////////////// Indication Leds ///////////////////////////
+///////////// InitSequence ///////////////////////////////
 
-const int redPin = 17;
-const int greenPin = 18;
-const int bluePin = 19;
 
-void PaintAllLeds(int color, int pin)
-{
-  digitalWrite(redPin, LOW);
-  digitalWrite(greenPin, LOW);
-  digitalWrite(bluePin, LOW);
-
-  digitalWrite(pin, HIGH);
-
-  for (int i = 0; i < leds.numPixels(); i++)
-    leds.setPixel(i, color);
+//4800
+void DefaultAnimation() {
+  int temp = leds.getPixel(0);
+  for (int i = 0; i < leds.numPixels() - 1; i++) {
+    leds.setPixel(i, leds.getPixel(i + 1));
+  }
+  leds.setPixel(leds.numPixels() - 1, temp);
 }
 
-void InitIndicationLeds()
-{
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
+void SetupDefaultAnimation() {
+  int hue = 0;
+  for (int i = 0; i < leds.numPixels(); i++) {
+    if (i % 10 == 0) {
+      hue = random(359);
+    }
 
-  digitalWrite(redPin, HIGH);
-  digitalWrite(greenPin, HIGH);
-  digitalWrite(bluePin, HIGH);
-
-  delay(500);
-
-  PaintAllLeds(leds.color(128, 0, 0), redPin);
-  leds.show();
-  delay(1000);
-  PaintAllLeds(leds.color(0, 128, 0), greenPin);
-  leds.show();
-  delay(1000);
-  PaintAllLeds(leds.color(0, 0, 128), bluePin);
-  leds.show();
-  delay(1000);
-
-  // we should leave only the green, as we have power, but no network or error
-  PaintAllLeds(leds.color(0, 0, 0), greenPin);
-  leds.show();
+    if ((i - i % 10) % 6 == 0) {  // color
+      int level = i % 10 > 5 ? 10 - i % 10 : i % 10;
+      level *= 2 * 4;
+      leds.setPixel(i, makeColor(hue, 100, level));
+    } else {
+      leds.setPixel(i, leds.color(0, 0, 0));
+    }
+  }
 }
 
 //////////////////////////////////////////////////////////
+///////////// Network ////////////////////////////////////
 
-void SendColorsToStrips()
-{
-  digitalWrite(bluePin, HIGH);
-  digitalWrite(redPin, LOW);
-  leds.show();
-}
-
-
-bool VerifyPacket(const uint8_t packetBuf[], int packetSize)
-{
+bool VerifyPacket(const uint8_t packetBuf[], int packetSize) {
   if (packetSize < LB_HEADER_SIZE)
     return false;
   if (memcmp(packetBuf, "LedBurn", 7) != 0)
@@ -160,64 +136,81 @@ bool VerifyPacket(const uint8_t packetBuf[], int packetSize)
   if (protocolVersion != 0)
     return false;
   int payloadLength = (packetSize - LB_HEADER_SIZE);
-  if ( (payloadLength % 3) != 0)
+  if ((payloadLength % 3) != 0)
     return false;
 
   return true;
 }
 
-PacketHeaderData ParsePacketHeader(const uint8_t packetBuf[], int packetSize)
-{
+PacketHeaderData ParsePacketHeader(const uint8_t packetBuf[], int packetSize) {
   PacketHeaderData phd;
 
-  phd.frameId = (*((const uint32_t *) (packetBuf + 8) ));
-  phd.segInFrame = (*((const uint32_t *) (packetBuf + 12) ));
+  phd.frameId = (*((const uint32_t *)(packetBuf + 8)));
+  phd.segInFrame = (*((const uint32_t *)(packetBuf + 12)));
 
-  phd.currSegId = (*((const uint32_t *) (packetBuf + 16) ));
-  phd.stripId = (*((const uint16_t *) (packetBuf + 20) ));
-  phd.pixelId = (*((const uint16_t *) (packetBuf + 22) ));
+  phd.currSegId = (*((const uint32_t *)(packetBuf + 16)));
+  phd.stripId = (*((const uint16_t *)(packetBuf + 20)));
+  phd.pixelId = (*((const uint16_t *)(packetBuf + 22)));
 
   phd.numOfPixels = (packetSize - LB_HEADER_SIZE) / 3;
 
   return phd;
 }
 
-void PaintLeds(const uint8_t packetBuf[], const PacketHeaderData &phd)
-{
+void TurnOffUnsetLeds(int segInFrame) {
+  // turn off leds without data
+  int counter = 0;
+  for (int i = 0; i < leds.numPixels(); i++) {
+    bool turnOff = true;
+    int stripId = (i - i % LEDS_PER_STRIP) / LEDS_PER_STRIP;
+    int pixelId = i - LEDS_PER_STRIP * stripId;
+    for (int j = 0; j < segInFrame; j++) {
+      // is pixel in segment range
+      if (stripId == receivedSegArr[j].stripId && pixelId >= receivedSegArr[j].pixelId && pixelId < receivedSegArr[j].pixelId + receivedSegArr[j].numOfPixels)
+        turnOff = false;
+    }
+    if (turnOff) {
+      counter++;
+      leds.setPixel(i, leds.color(0, 0, 0));
+    }
+  }
+  // Serial.println(String(leds.numPixels() - counter) + " out of " + String(leds.numPixels()));
+}
+
+void PaintLeds(const uint8_t packetBuf[], const PacketHeaderData &phd) {
   // avoid overrun the allowed buffer
   if (phd.stripId >= 8)
     return;
   if (phd.pixelId >= MAX_PIXELS)
     return;
-  int numOfPixels = min(phd.numOfPixels, (uint16_t)(MAX_PIXELS - phd.pixelId) ); // MAX_PIXELS - phd.pixelId > 0
+  int numOfPixels = min(phd.numOfPixels, (uint16_t)(MAX_PIXELS - phd.pixelId));  // MAX_PIXELS - phd.pixelId > 0
 
   // we have 3 pointers to follow:
   // 1. the current led index
   // 2. the source of the rgb to copy from
   // 3. the index in the leds array to copy into
   const uint8_t *sourcePointer = (const uint8_t *)packetBuf + LB_HEADER_SIZE;
-  uint32_t destLedIndex = (phd.stripId * LEDS_PER_STRIP) + phd.pixelId; // pixel index, not channel index
+  uint32_t destLedIndex = (phd.stripId * LEDS_PER_STRIP) + phd.pixelId;  // pixel index, not channel index
 
-  for (int i = 0; i < numOfPixels; i++)
-  {
+  for (int i = 0; i < numOfPixels; i++) {
     leds.setPixel(destLedIndex, *(sourcePointer + 0), *(sourcePointer + 1), *(sourcePointer + 2));
     sourcePointer += 3;
     destLedIndex++;
   }
 }
 
-void ResetCounter(uint32_t newFrameId)
-{
+void ResetCounter(uint32_t newFrameId) {
   currentFrame = newFrameId;
   numOfReceivedSegments = 0;
-  for (int i = 0; i < MAX_SUPPORTED_SEGMENTS; i++)
-    receivedSegArr[i] = false;
+  for (int i = 0; i < MAX_SUPPORTED_SEGMENTS; i++) {
+    receivedSegArr[i] = EmptyPacketHeaderData;
+    receivedSegArrBool[i] = false;
+  }
 }
 
 // return true if packet is ok.
 // return false if packet should be ignored
-bool BeforePaintLeds(const PacketHeaderData &phd)
-{
+bool BeforePaintLeds(const PacketHeaderData &phd) {
   if (phd.segInFrame >= MAX_SUPPORTED_SEGMENTS || phd.currSegId >= phd.segInFrame)
     return false;
 
@@ -229,31 +222,32 @@ bool BeforePaintLeds(const PacketHeaderData &phd)
   // do the math with int64, to avoid overflows
   // unless it's very old, in which case, assume the sender restarted and use it
   int64_t diffFromCurrent = (int64_t)phd.frameId - (int64_t)currentFrame;
-  if (diffFromCurrent > -500 && diffFromCurrent < 0) // 500 is 10 seconds in 50HZ
+  if (diffFromCurrent > -500 && diffFromCurrent < 0)  // 500 is 10 seconds in 50HZ
     return false;
 
   // if we are here, then this frame is not what we expected, but it is not frame from udp re-order.
   // so we change our reference point to it!
+  // Serial.println("Oh no");
   ResetCounter(phd.frameId);
-  SendColorsToStrips(); // use the leds we already recived
+  leds.show();  // use the leds we already recived
   return true;
 }
 
-void AfterPaintLeds(const PacketHeaderData &phd)
-{
-  if (receivedSegArr[phd.currSegId])
-  {
+void AfterPaintLeds(const PacketHeaderData &phd) {
+  if (receivedSegArrBool[phd.currSegId]) {
     // we already have this segment. this is a duplicate packet!
     return;
   }
 
-  receivedSegArr[phd.currSegId] = true;
+  receivedSegArr[phd.currSegId] = phd;
+  receivedSegArrBool[phd.currSegId] = true;
   numOfReceivedSegments++;
 
-  if (numOfReceivedSegments >= phd.segInFrame)
-  {
+  if (numOfReceivedSegments >= phd.segInFrame) {
+    // send data to leds
+    TurnOffUnsetLeds(phd.segInFrame);
     ResetCounter(phd.frameId + 1);
-    SendColorsToStrips();
+    leds.show();
   }
 }
 
@@ -268,48 +262,54 @@ void setup() {
   Ethernet.begin(mac, ip);
   Udp.begin(localPort);
 
-  InitIndicationLeds();
+  SetupDefaultAnimation();
 }
 
+//////////////////////////////////////////////////////////
+///////////// Main Loop //////////////////////////////////
 
-long previousShow = 0;
+long lastPacketTime = 0;
+long lastLedsShow = 0;
+bool networkMode = false;
+
+
 void loop() {
   // if there's data available, read a packet
   int packetSize = Udp.parsePacket();
-  digitalWrite(bluePin, LOW);
 
   if (packetSize > 0) {
-     Serial.println(packetSize);
-    
-    if (packetSize > MAX_PACKET_SIZE)
-    {
-      digitalWrite(redPin, HIGH);
+    // Serial.println(packetSize);
+    lastPacketTime = millis();
+    networkMode = true;
+
+    if (packetSize > MAX_PACKET_SIZE) {
       return;
     }
 
     uint8_t tempBuf[MAX_PACKET_SIZE];
     Udp.read((uint8_t *)tempBuf, MAX_PACKET_SIZE);
 
-    if (!VerifyPacket(tempBuf, packetSize))
-    {
-      digitalWrite(redPin, HIGH);
+    if (!VerifyPacket(tempBuf, packetSize)) {
       return;
     }
 
     PacketHeaderData phd = ParsePacketHeader(tempBuf, packetSize);
-    if (!BeforePaintLeds(phd))
-    {
-      digitalWrite(redPin, HIGH);
+    // Serial.println("FRAME - "+ String(phd.frameId)  + " - SEGMENT RECIEVED - " + String(phd.currSegId) + " out of " + String(phd.segInFrame) 
+    // + " segments: strip-" + String(phd.stripId) + " pixel-" + String(phd.pixelId) + " numOfLeds-" + String(phd.numOfPixels));
+    if (!BeforePaintLeds(phd)) {
       return;
     }
     PaintLeds(tempBuf, phd);
     AfterPaintLeds(phd);
-    previousShow = millis();
+    lastLedsShow = millis();
 
-  } else if (millis() - previousShow > 1000) {
-    PaintAllLeds(leds.color(0, 0, 0), bluePin);
+  } else if (networkMode && millis() - lastPacketTime > 5000) {
+    networkMode = false;
+    SetupDefaultAnimation();
+
+  } else if (!networkMode && millis() - lastLedsShow > 30) {
+    DefaultAnimation();
     leds.show();
-    previousShow = millis();
+    lastLedsShow = millis();
   }
 }
-
